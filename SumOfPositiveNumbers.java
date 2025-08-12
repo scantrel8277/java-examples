db058db1d36961a45fec6fa12ad95888a31594f7
scantrel8277 <simon.cantrell@gmail.com>
2025-08-12 15:27:38 -0500

    Add files via upload

---
+++ SumOfPositiveNumbers.java
@@ -0,0 +1,25 @@
+// Java program to find the sum of positive numbers
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+      
+    int sum = 0;
+    int number = 0;
+
+    // create an object of Scanner class
+    Scanner input = new Scanner(System.in);
+	   
+    // do...while loop continues 
+    // until entered number is positive
+    do {
+      // add only positive numbers
+      sum += number;
+      System.out.println("Enter a number");
+      number = input.nextInt();
+    } while(number >= 0); 
+	   
+    System.out.println("Sum = " + sum);
+    input.close();
+  }
+}
